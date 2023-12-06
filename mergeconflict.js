d8060f0d67ea6d194c5c02a102535fb91d70b86b
unknown <centristy@gmail.com>
2023-12-06 12:34:14 +0900

    new webpage

--- mergeconflict.js
+++
@@ -1,2 +0,0 @@
-console.log("I made a change! I'm going to try to cause a merge conflict! Please let it work!!")
-console.log("Will this cause an error")
---
+++ style.js
@@ -0,0 +1,19 @@
+
+
+
+
+function randomRGB() {
+    const r = Math.floor(Math.random()*100+150);
+    const b = Math.floor(Math.random()*100+150);
+    const g = Math.floor(Math.random()*256+150);
+    return `rgb(${r},${b},${g})`
+}
+
+const letters = document.querySelectorAll('.letter');
+
+
+setInterval(function(){
+for (let letter of letters){
+    letter.style.color = randomRGB();
+}
+},3000)
