46ac52fdfa1eb9657d908a634fe15536f97a0c70
unknown <centristy@gmail.com>
2023-12-07 00:33:09 +0900

    added some event buttons

--- style.js
+++ style.js
@@ -4,17 +4,32 @@ const h1 = document.querySelectorAll('h1');
 
 
 function randomRGB() {
-    const r = Math.floor(Math.random()*100+150);
-    const b = Math.floor(Math.random()*100+150);
-    const g = Math.floor(Math.random()*256+150);
+    const r = Math.floor(Math.random()*150+100);
+    const b = Math.floor(Math.random()*150+100);
+    const g = Math.floor(Math.random()*150+100);
     return `rgb(${r},${b},${g})`
 }
 
-const letters = document.querySelectorAll('.letter');
+function stopcolor(){
+    document.querySelector(".letter").className = "paused";
+    
+
+}
 
 
-setInterval(function(){
-for (let letter of letters){
+function gocolor(){
+    document.querySelector(".paused").className = "letter";
+    
+
+}
+
+
+
+setInterval(function (){
+
+    const letters = document.querySelectorAll('.letter');
+    for (let letter of letters){
     letter.style.color = randomRGB();
 }
 },1000)
+
